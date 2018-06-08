350b7e6c5fa3cc0f7cf761e13de4a463ecc50d7e
Amey Khairnar <ameykhairnar99@gmail.com>
2018-06-08 23:28:04 +0530

    removed footer

--- scripts/jq.js
+++ scripts/jq.js
@@ -4,8 +4,8 @@ $(document).ready(
             function(){
                 $(".eduh").toggleClass("opa")
                 $("#edu").slideToggle(270)
-                $("#skills, #act, #hob, #git").hide(270)
-                $(".skillsh, .hobh, .acth, .gith, .allh").removeClass("selected")
+                $("#skills, #act, #hob, #git, #con").hide(270)
+                $(".skillsh, .hobh, .acth, .gith, .conh").removeClass("selected")
                 $(".eduh").addClass("selected")
             }
         )
@@ -14,8 +14,8 @@ $(document).ready(
             function(){
                 $(".skillsh").toggleClass("shrink")
                 $("#skills").slideToggle(270)
-                $("#edu, #act, #hob, #git").hide(270)
-                $(".eduh, .hobh, .acth, .gith, .allh").removeClass("selected")
+                $("#edu, #act, #hob, #git, #con").hide(270)
+                $(".eduh, .hobh, .acth, .gith, .conh").removeClass("selected")
                 $(".skillsh").addClass("selected")
             }
         )
@@ -24,8 +24,8 @@ $(document).ready(
             function(){
                 $(".acth").toggleClass("shrink")
                 $("#act").slideToggle(270)
-                $("#edu, #skills, #hob, #git").hide(270)
-                $(".eduh, .hobh, .skillsh, .gith, .allh").removeClass("selected")
+                $("#edu, #skills, #hob, #git, #con").hide(270)
+                $(".eduh, .hobh, .skillsh, .gith, .conh").removeClass("selected")
                 $(".acth").addClass("selected")
             }
         )
@@ -34,8 +34,8 @@ $(document).ready(
             function(){
                 $(".hobh").toggleClass("shrink")
                 $("#hob").slideToggle(270)
-                $("#edu, #skills, #act, #git").hide(270)
-                $(".eduh, .skillsh, .acth, .gith, .allh").removeClass("selected")
+                $("#edu, #skills, #act, #git, #con").hide(270)
+                $(".eduh, .skillsh, .acth, .gith, .conh").removeClass("selected")
                 $(".hobh").addClass("selected")
             }
         )
@@ -44,23 +44,23 @@ $(document).ready(
             function(){
                 $(".gith").toggleClass("shrink")
                 $("#git").slideToggle(270)
-                $("#edu, #skills, #hob, #act").hide(270)
-                $(".eduh, .hobh, .acth, .skillsh, .allh").removeClass("selected")
+                $("#edu, #skills, #hob, #act, #con").hide(270)
+                $(".eduh, .hobh, .acth, .skillsh, .conh").removeClass("selected")
                 $(".gith").addClass("selected")
             }
         )
 
-        $(".allh").click(
+        $(".conh").click(
             function(){
-                $(".allh").toggleClass("shrink")
-                $(".root").slideUp(1)
-                $(".root").toggle(220)
+                $(".conh").toggleClass("shrink")
+                $("#con").slideToggle(270)
+                $("#edu, #skills, #hob, #act, #git").hide(270)
                 $(".eduh, .hobh, .acth, .gith, .skillsh").removeClass("selected")
-                $(".allh").addClass("selected")
+                $(".conh").addClass("selected")
             }
         )
 
-        $(".skillsh, .hobh, .acth, .gith, .allh").removeClass("selected")
+        $(".skillsh, .hobh, .acth, .gith, .conh").removeClass("selected")
         $(".eduh").addClass("selected")
         $(".card-header, #edu, .panel, #footer, #end").fadeIn(1800)
     }
